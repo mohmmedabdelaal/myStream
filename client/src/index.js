8f2e60b57f4e22ac1afed0ad07d6215b25d09de1
Moelaal <medo.elaal@gmail.com>
2022-07-19 03:36:45 +0200

    env

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ ReactDOM.render(
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
+    cacheLocation="localstorage"
   >
     <BrowserRouter>
       <App />
