66ad23f080432fd1b499ef950cef2c5ad1bdb6a6
Moelaal <medo.elaal@gmail.com>
2022-07-20 07:43:41 +0200

    setup redux form

--- client/src/components/streams/CreateStream.js
+++ client/src/components/streams/CreateStream.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 function CreateStream() {
+  const renderForm = ({ input, label }) => {
+    console.log(input);
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
   return (
     <section className="ui container">
-      <div className="section-center">Create</div>
+      <div className="section-center">
+        <form className="ui form">
+          <Field name="title" component={renderForm} label="Enter title" />
+          <Field
+            name="description"
+            component={renderForm}
+            label="Enter description"
+          />
+        </form>
+      </div>
     </section>
   );
 }
 
-export default CreateStream;
+export default reduxForm({ form: 'createStream' })(CreateStream);
